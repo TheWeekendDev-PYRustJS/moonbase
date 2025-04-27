1e8b134ea21453d1ae660b6d061f1f666473fc44
TheWeekendDev-PYRustJS <Wildgenius99@gmail.com>
2025-04-26 20:20:14 -0400

    Updated Home page music with stop button.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,15 @@ import "../styles.css"; // Import CSS for styling
 
 
 const Home = () => {
-    const [play, { stop, isPlaying }] = useSound(backgroundMusic, {loop: false});
+    const [play, { stop }] = useSound(backgroundMusic, {loop: false});
 
     return (
         <div className="home-container">
             {/* Sci-Fi Banner */}
             <div className="banner">
                 <h1>The Moon</h1>
-                    <button onClick={isPlaying ? stop : play}>
-                        {isPlaying ? 'Stop Music' : 'Play Music'}
-                    </button>
+                <button onClick={() => play()}>Play</button>
+                <button onClick={() => stop()}>Stop</button>
             </div>
 
         </div>
